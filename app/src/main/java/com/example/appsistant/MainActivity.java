14eac9b0d2eacdd766046e910c85b74736847c95
Joon <yeonjoon.developer@gmail.com>
2021-10-12 14:20:40 +1000

    test

--- app/src/main/java/com/example/appsistant/MainActivity.java
+++ app/src/main/java/com/example/appsistant/MainActivity.java
@@ -1,14 +1,10 @@
 package com.example.appsistant;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,17 +17,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         LinearLayout calendar = findViewById(R.id.calendar);
-        calendar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d(TAG, "onClick: Calendar");
+        calendar.setOnClickListener(view -> {
+            Log.d(TAG, "onClick: Calendar");
 
-                Intent intent = new Intent(MainActivity.this, CalendarActivity.class);
-                startActivity(intent);
-            }
+            Intent intent = new Intent(MainActivity.this, CalendarActivity.class);
+            startActivity(intent);
         });
-
-
 
 
 
