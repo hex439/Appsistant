77f26718e15ce76b825d1018bcf91694a5c8d633
Eugene Nam jc463628 <54092140+jc463628@users.noreply.github.com>
2021-10-19 01:21:47 +1000

    Update Camera app

--- app/src/main/java/com/example/appsistant/MainActivity.java
+++ app/src/main/java/com/example/appsistant/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.appsistant;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.ComponentName;
+import android.content.ContentUris;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -26,7 +31,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Log.d(TAG, "onClick: Calendar");
 
-                Intent intent = new Intent(MainActivity.this, CalendarActivity.class);
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivity(intent);
                 System.out.println("test");
             }
