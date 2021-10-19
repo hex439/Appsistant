9862b04d255ac354423bd9c7f1b58e7c95c892db
Eugene Nam jc463628 <54092140+jc463628@users.noreply.github.com>
2021-10-19 14:20:29 +1000

    Clean code

--- app/src/main/java/com/example/appsistant/MainActivity.java
+++ app/src/main/java/com/example/appsistant/MainActivity.java
@@ -2,16 +2,11 @@ package com.example.appsistant;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.provider.AlarmClock;
 import android.provider.ContactsContract;
 import android.util.Log;
 import android.widget.Button;
-import android.widget.ImageButton;
-import android.provider.MediaStore;
-import android.view.View;
-import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,27 +32,21 @@ public class MainActivity extends AppCompatActivity {
         Button calendar_Btn = findViewById(R.id.calendarBtn);
         Button cameraButton = findViewById(R.id.cameraBtn);
 
-        calendar_Btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d(TAG, "onClick: Calendar");
+        calendar_Btn.setOnClickListener(view -> {
+            Log.d(TAG, "onClick: Calendar");
 
-                Intent intent = new Intent(Intent.ACTION_EDIT);
-                intent.setType("vnd.android.cursor.item/event");
-                intent.putExtra("title", "Some title");
-                intent.putExtra("description", "Some description");
-                intent.putExtra("beginTime", 0);
-                intent.putExtra("endTime", 0);
-                startActivity(intent);
-            }
+            Intent intent = new Intent(Intent.ACTION_EDIT);
+            intent.setType("vnd.android.cursor.item/event");
+            intent.putExtra("title", "Some title");
+            intent.putExtra("description", "Some description");
+            intent.putExtra("beginTime", 0);
+            intent.putExtra("endTime", 0);
+            startActivity(intent);
         });
 
-        cameraButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-                startActivity(intent);
-            }
+        cameraButton.setOnClickListener(v -> {
+            Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+            startActivity(intent);
         });
     }
 }
