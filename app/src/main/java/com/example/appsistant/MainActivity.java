eff9fd0fed2df67f0d04df91a16c5d6a5050d086
Eugene Nam jc463628 <54092140+jc463628@users.noreply.github.com>
2021-10-19 12:23:10 +1000

    Added camera button functionality
    Camera button function finished.

--- app/src/main/java/com/example/appsistant/MainActivity.java
+++ app/src/main/java/com/example/appsistant/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.appsistant;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.annotation.SuppressLint;
 import android.content.ComponentName;
 import android.content.ContentUris;
 import android.content.Intent;
@@ -12,6 +13,7 @@ import android.provider.CalendarContract;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -25,12 +27,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        LinearLayout calendar = findViewById(R.id.calendar);
-        calendar.setOnClickListener(new View.OnClickListener() {
+        Button calendar_Btn = findViewById(R.id.calendarBtn);
+        Button cameraButton = findViewById(R.id.cameraBtn);
+
+        calendar_Btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d(TAG, "onClick: Calendar");
 
+            }
+        });
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
                 intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivity(intent);
