6e71a82ef6b5e72dd83fb1f65186148d08338f8e
khiemnguyen2609 <74776550+hex439@users.noreply.github.com>
2021-10-19 12:55:55 +1000

    add code for contacts

--- app/src/main/java/com/example/appsistant/MainActivity.java
+++ app/src/main/java/com/example/appsistant/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -21,14 +23,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         LinearLayout calendar = findViewById(R.id.calendar);
-        calendar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d(TAG, "onClick: Calendar");
+        calendar.setOnClickListener(view -> {
+            Log.d(TAG, "onClick: Calendar");
 
-                Intent intent = new Intent(MainActivity.this, CalendarActivity.class);
-                startActivity(intent);
-            }
+            Intent intent = new Intent(MainActivity.this, CalendarActivity.class);
+            startActivity(intent);
+        });
+
+        ImageButton contacts = findViewById(R.id.button_contacts);
+        contacts.setOnClickListener(v -> {
+            Intent intent= new Intent(Intent.ACTION_PICK,  ContactsContract.Contacts.CONTENT_URI);
+            startActivityForResult(intent, 1);
         });
 
 
