2550680535f6d9775b55c927c56fd0280a272a70
Lucy <lucy.smith2@my.jcu.edu.au>
2021-11-02 20:48:20 +1000

    Fixed calendar view bug

--- app/src/main/java/com/example/appsistant/CalendarActivity.java
+++ app/src/main/java/com/example/appsistant/CalendarActivity.java
@@ -20,18 +20,12 @@ public class CalendarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_calendar);
 
         ImageButton viewCalendar = findViewById(R.id.view_calendar);
-        ImageButton createEvent = findViewById(R.id.create_event);
 
         viewCalendar.setOnClickListener(view -> {
             Log.d(TAG, "onClick: View Calendar Button");
-            Log.d("CalendarActivity", "Launched: calendarFragment");
 
-            Intent intent = new Intent(Intent.ACTION_EDIT);
+            Intent intent = new Intent(Intent.ACTION_VIEW);
             intent.setType("vnd.android.cursor.item/event");
-            intent.putExtra("title", "Some title");
-            intent.putExtra("description", "Some description");
-            intent.putExtra("beginTime", 0);
-            intent.putExtra("endTime", 0);
             startActivity(intent);
 
         });
